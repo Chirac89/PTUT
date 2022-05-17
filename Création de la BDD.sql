9c1f7511fdd315816f471002cf31cf5d936c692f
edvardFH <97302653+edvardFH@users.noreply.github.com>
2022-05-17 09:08:37 +0200

    sql

--- Création de la BDD.sql
+++ Création de la BDD.sql
@@ -113,81 +113,89 @@ CREATE TABLE logs (
 /*===============================================================================================================*/
 
 CREATE TABLE customers (
-    PK_customer_ID CHAR(15),
+    PK_customer_ID INT NOT NULL AUTO_INCREMENT,
     
     customer_lastName VARCHAR(255) NOT NULL,
     customer_firstName VARCHAR(255) NOT NULL,
     customer_email VARCHAR(255) NOT NULL,
     customer_passwordHash VARCHAR(255) NOT NULL,
-    customer_accountCreationDate DATE NOT NULL,
+    customer_accountCreationDate DATE NOT NULL DEFAULT CURRENT_DATE,
     customer_birthday DATE NOT NULL,
     
-    CONSTRAINT C_PK_customer PRIMARY KEY (PK_customer_ID),
+    PRIMARY KEY (PK_customer_ID),
     CONSTRAINT C_UC_customer_email UNIQUE (customer_email)
 );
 
+ALTER TABLE customers AUTO_INCREMENT=1000000001;
+
 
 CREATE TABLE categories (
-    PK_cat_ID CHAR(15),
+    PK_cat_ID INT NOT NULL AUTO_INCREMENT,
     
     cat_name VARCHAR(255) NOT NULL,
     
-    CONSTRAINT C_PK_category PRIMARY KEY (PK_cat_ID),
+    PRIMARY KEY (PK_cat_ID),
     CONSTRAINT C_UC_cat_name UNIQUE (cat_name)
 );
 
+ALTER TABLE categories AUTO_INCREMENT=1001;
+
 
 CREATE TABLE subcategories (
-    PK_subcat_ID CHAR(15),
-    FK_subcat_cat_ID CHAR(15),
+    PK_subcat_ID INT NOT NULL AUTO_INCREMENT,
+    FK_subcat_cat_ID INT NOT NULL,
     
     subcat_name VARCHAR(255) NOT NULL,
     
-    CONSTRAINT C_PK_subcategory PRIMARY KEY (PK_subcat_ID),
+    PRIMARY KEY (PK_subcat_ID),
     CONSTRAINT C_FK_subcategory_cat_ID FOREIGN KEY (FK_subcat_cat_ID) REFERENCES categories(PK_cat_ID),
     CONSTRAINT C_UC_subcat_name UNIQUE (subcat_name)
 );
 
+ALTER TABLE subcategories AUTO_INCREMENT=1001;
+
 
 CREATE TABLE products (
-    PK_product_ID CHAR(15),
-    FK_product_cat_ID CHAR(15) NOT NULL,
-    FK_product_subcat_ID CHAR(15),
+    PK_product_ID INT NOT NULL AUTO_INCREMENT,
+    FK_product_cat_ID INT NOT NULL,
+    FK_product_subcat_ID INT,
     
     product_name VARCHAR(255) NOT NULL,
     product_price FLOAT NOT NULL,
     product_description TEXT,
-    product_discountRate INT NOT NULL,
     product_alertThreshold INT NOT NULL,
     product_stock INT NOT NULL,
     
-    CONSTRAINT C_PK_product PRIMARY KEY (PK_product_ID),
+    PRIMARY KEY (PK_product_ID),
     CONSTRAINT C_FK_product_cat_ID FOREIGN KEY (FK_product_cat_ID) REFERENCES categories(PK_cat_ID),
     CONSTRAINT C_FK_product_subcat_ID FOREIGN KEY (FK_product_subcat_ID) REFERENCES subcategories(PK_subcat_ID)
 );
 
+ALTER TABLE products AUTO_INCREMENT=100001;
 
-CREATE TABLE promotionalOperations (
-    PK_promoOperation_ID CHAR(15),
-    FK_promoOperation_product_ID CHAR(15) NOT NULL,
+CREATE TABLE promotions (
+    PK_FK_promotion_product_ID INT NOT NULL,
+    PK_promotion_start DATETIME NOT NULL,
+    PK_promotion_end DATETIME NOT NULL,
     
-    promoOperation_name VARCHAR(255),
-    promoOperation_start DATETIME NOT NULL,
-    promoOperation_end DATETIME NOT NULL,
+    promotion_name VARCHAR(255),
+    promotion_discountRate INT NOT NULL,
     
-    CONSTRAINT C_PK_promotionalOperation PRIMARY KEY (PK_promoOperation_ID),
-    CONSTRAINT C_FK_promoOperation_product_ID FOREIGN KEY (FK_promoOperation_product_ID) REFERENCES products(PK_product_ID)
+    PRIMARY KEY (PK_FK_promotion_product_ID, PK_promotion_start, PK_promotion_end),
+    CONSTRAINT C_FK_promotion_product_ID FOREIGN KEY (PK_FK_promotion_product_ID) REFERENCES products(PK_product_ID)
 );
 
 
 CREATE TABLE pictures (
-    PK_picture_ID CHAR(10),
-    FK_picture_product_ID CHAR(10),
+    PK_picture_ID INT NOT NULL AUTO_INCREMENT,
+    FK_picture_product_ID INT NOT NULL,
     
     picture_path VARCHAR(255) NOT NULL,
     
-    CONSTRAINT C_PK_picture PRIMARY KEY (PK_picture_ID),
+    PRIMARY KEY (PK_picture_ID),
     CONSTRAINT C_FK_picture_product_ID FOREIGN KEY (FK_picture_product_ID) REFERENCES products(PK_product_ID)
 );
+
+ALTER TABLE pictures AUTO_INCREMENT=100001;
 
 
